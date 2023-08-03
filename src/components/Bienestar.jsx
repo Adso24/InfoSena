1b5017a208d1960591974ad5dde82eac9d385aed
StevenDev <estiventrejos13@hotmail.com>
2023-08-03 17:42:10 -0500

    bienestar faltante de footer y revisión de colores, commit anterior no trackeó los archivos multimedia

---
+++ src/components/Bienestar.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../Css/Bienestar.css";
+import { Slider } from "../components/Slider";
+
+export const Bienestar = () => {
+  return (
+    <>
+      <header>
+        <nav>
+          <div className="navegacion-areas margen-nav">
+            <Link to="/">Inicio</Link>
+
+            <Link to="/bienestar">Bienestar</Link>
+
+            <Link to="/biblioteca">Biblioteca</Link>
+
+            <Link to="/directivas">Directivas</Link>
+
+            <Link to="/areas">Áreas</Link>
+          </div>
+        </nav>
+      </header>
+
+      <section>
+        <div className="contenedor-padre-bienestar">
+          <div className="contenedor-bienestar">
+            <div className="imagen1-bienestar">
+              <img src="/src/images/bienestar-header.png" alt="" />
+            </div>
+            <h1>
+              BIENVENID@S A <br /> BIENESTAR AL <br /> APRENDIZ
+            </h1>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="contenedor-bienestar-seccion-2">
+          <div className="parte-izquierda-bienestar">
+            <h1 className="titulo-seccion-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+              tempore deleniti maxime laborum delectus dolorem praesentium
+              repellat cupiditate iure quo?
+            </h1>
+            <a href="#" className="lightbox-seccion-2">
+              Ver más
+            </a>
+          </div>
+          <div className="imagen-2-bienestar">
+            <img src="/src/images/bienestar-header.png" alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="contenedor-padre-bienestar-seccion-3">
+          <div className="circulo-container">
+            <div className="circulo circulo-1">
+              <img src="/src/images/integrante-1.png" alt="Image 1" />
+            </div>
+            <div className="circulo circulo-2">
+              <img src="/src/images/integrante-2.png" alt="Image 2" />
+            </div>
+            <div className="circulo circulo-3">
+              <img src="/src/images/integrante-3.png" alt="Image 3" />
+            </div>
+            <div className="circulo circulo-4">
+              <img src="/src/images/integrante-4.jpg" alt="Image 4" />
+            </div>
+          </div>
+          <div className="parte-izquierda-bienestar">
+            <h1 className="titulo-seccion-2">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed,
+              tempore deleniti maxime laborum delectus dolorem praesentium
+              repellat cupiditate iure quo?
+            </h1>
+            <a href="#" className="lightbox-seccion-2">
+              Ver más
+            </a>
+          </div>
+        </div>
+      </section>
+
+      <section>
+        <div className="slider-bienestar">
+        <Slider />
+        </div>
+      </section>
+    </>
+  );
+};
---
+++ src/components/Slider.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "/src/Css/Slider-bienestar.css";
+
+export const Slider = () => {
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [autoPlay, setAutoPlay] = useState(true);
+
+  const handlePrev = () => {
+    setCurrentIndex(
+      (prevIndex) => (prevIndex - 1 + images.length) % images.length
+    );
+    setAutoPlay(false);
+  };
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    setAutoPlay(false);
+  };
+
+  const images = [
+    "/src/images/bienestar-header.png",
+    "/src/images/bienestar-header.png",
+    "/src/images/bienestar-header.png",
+    "/src/images/bienestar-header.png",
+  ];
+
+  useEffect(() => {
+    if (autoPlay) {
+      const interval = setInterval(() => {
+        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+      }, 3000);
+
+      return () => clearInterval(interval);
+    }
+  }, [autoPlay, images.length]);
+
+  return (
+    <>
+      <div className="slider-container">
+        <div className="header-slider-bienestar">
+          <div className="titulo-slider">
+            <p>DESCUBRE TAMBIÉN:</p>
+          </div>
+          <div className="slider-arrows">
+            <button onClick={handlePrev}>
+              <span class="material-symbols-outlined">arrow_back</span>
+            </button>
+            <button onClick={handleNext}>
+              <span class="material-symbols-outlined">arrow_forward</span>
+            </button>
+          </div>
+        </div>
+        <Carousel className="carrusel"
+          selectedItem={currentIndex}
+          onChange={() => setAutoPlay(true)}
+          infiniteLoop
+          autoPlay={autoPlay}
+          showStatus={false}
+          showArrows={false}
+          showIndicators={false}
+          showThumbs={false}
+          stopOnHover={false}
+          swipeable={false}
+        >
+          {images.map((image, index) => (
+            <div key={index} className="slider-item">
+              <img src={image} alt={`Image ${index + 1}`} />
+              <h1>Hola</h1>
+            </div>
+          ))}
+        </Carousel>
+      </div>
+    </>
+  );
+};
